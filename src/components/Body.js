ebe253c240a898a053565dbebd50f4e5778c181f
Abhishek Bondre <abhibondre27@gmail.com>
2023-10-14 13:22:52 +0530

    add hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import resList from "../utils/mockData";
 import RestaurentCard from "./RestaurentCard";
 
 const Body = () => {
+  const [restaurentList, setRestaurentList] = useState(resList);
+
   return (
     <>
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+          <button
+            className="filter-btn"
+            onClick={() => {
+              filterResList = restaurentList.filter(
+                (res) => res.info.avgRating > 4
+              );
+              setRestaurentList(filterResList);
+            }}
+          >
+            Top Rated Restaurents
+          </button>
+        </div>
         <div className="res-container">
-          {Array.isArray(resList) &&
-            resList.map((restaurent) => {
+          {Array.isArray(restaurentList) &&
+            restaurentList.map((restaurent) => {
               return (
                 <RestaurentCard key={restaurent.info.id} resObj={restaurent} />
               );
